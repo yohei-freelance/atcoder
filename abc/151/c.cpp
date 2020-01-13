e9501254331487ccec54372166efef6aa1eca66c
yohei-freelance <n-yohei-1717@g.ecc.u-tokyo.ac.jp>
2020-01-13 14:45:13 +0900

    commit

--- abc/151/c.cpp
+++ abc/151/c.cpp
@@ -13,13 +13,33 @@ using graph = vector<vector<edge>>;
 
 int main(){
     int n, m; cin >> n >> m;
-    vector<int> wa(m);
-    vector<int> ac(m);
+    vector<int> wa(n);
+    vector<int> ac(n);
+    for(int i=0; i<n; i++){
+        wa[i] = 0;
+        ac[i] = 0;
+    }
     for(int i=0; i<m; i++){
         int problem_num; cin >> problem_num;
-        int problem_result; cin >> problem_result;
-        if(problem_result == 'WA'){
-            
+        str problem_result; cin >> problem_result;
+        if (problem_result == "WA"){
+            if (ac[problem_num-1] == 0){
+                wa[problem_num-1] += 1;
+            }
+        }else{
+            if (ac[problem_num-1] == 0){
+                ac[problem_num-1] += 1;
+            }
         }
     }
+    int ans_wa = 0;
+    int ans_ac = 0;
+    for(int i=0; i<n; i++){
+        if (ac[i] != 0){
+            ans_wa += wa[i];
+            ans_ac += 1;
+        }
+    }
+    cout << ans_ac << " " << ans_wa << endl;
+    return 0;
 }
