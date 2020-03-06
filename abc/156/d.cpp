8ca6c075e234aedc46e129773bfb27fd49dcb853
yohei-freelance <n-yohei-1717@g.ecc.u-tokyo.ac.jp>
2020-03-06 21:56:44 +0900

    commit

--- abc/156/d.cpp
+++ abc/156/d.cpp
@@ -16,28 +16,7 @@ struct edge{
 };
 using graph = vector<vector<edge>>;
 
-ll fac[MAX], finv[MAX], inv[MAX];
-ll count_prs;
-
-void COMinit() {
-    fac[0] = fac[1] = 1;
-    finv[0] = finv[1] = 1;
-    inv[1] = 1;
-    for (int i = 2; i < MAX; i++){
-        fac[i] = fac[i - 1] * i % MOD;
-        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
-        finv[i] = finv[i - 1] * inv[i] % MOD;
-    }
-}
-
-ll COM(int n, int k){
-    if (n < k) return 0;
-    if (n < 0 || k < 0) return 0;
-    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
-}
-
 ll PowMod_RepeatSquaring(ll N, ll P, ll M){
-    ++count_prs;
     if(P==0) return 1;
     if(P%2==0){
         ll t = PowMod_RepeatSquaring(N, P/2, M);
@@ -46,19 +25,55 @@ ll PowMod_RepeatSquaring(ll N, ll P, ll M){
     return N * PowMod_RepeatSquaring(N, P-1, M);
 }
 
+ll modinv(ll a, ll m) {
+    ll b = m, u = 1, v = 0;
+    while (b) {
+        ll t = a / b;
+        a -= t * b; swap(a, b);
+        u -= t * v; swap(u, v);
+    }
+    u %= m;
+    if (u < 0) u += m;
+    return u;
+}
+
 int main(){
-    COMinit();
     ll n, a, b; cin >> n >> a >> b;
     ll ans = PowMod_RepeatSquaring(2, n, MOD) - 1;
-    ans -= COM(n, a);
-    ans -= COM(n, b);
-    if(ans > 0) cout << ans << endl;
-    else{
-        ans += MOD;
-        if(ans < 0){
-            ans += MOD;
-        }
-        cout << ans << endl;
+    // nCaとnCbを求める
+    ll a_fact = 1;
+    ll b_fact = 1;
+    ll a_x = 1;
+    ll b_x = 1;
+    for(int i=1; i<=a; i++){
+        a_fact *= i;
+        a_fact %= MOD;
     }
+    REP(i, a){
+        a_x *= n-i;
+        a_x %= MOD;
+    }
+    for(int i=1; i<=b; i++){
+        b_fact *= i;
+        b_fact %= MOD;
+    }
+    REP(i, b){
+        b_x *= n-i;
+        b_x %= MOD;
+    }
+    ll a_com = a_x * modinv(a_fact, MOD) % MOD;
+    ll b_com = b_x * modinv(b_fact, MOD) % MOD;
+    ans -= a_com;
+    ans -= b_com;
+    if(ans < 0){
+        bool minus = true;
+        while(minus){
+            ans += MOD;
+            if(ans >= 0){
+                minus = false;
+            }
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
